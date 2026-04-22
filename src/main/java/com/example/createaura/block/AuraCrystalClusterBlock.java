3bb94312a78f0109b5f65b6800d81e2cb1f9c099
wsp12345264 <ezwfdxgrt@126.com>
2026-04-22 22:01:54 +0800

    Add src/main/java/com/example/createaura/block/AuraCrystalClusterBlock.java

---
+++ src/main/java/com/example/createaura/block/AuraCrystalClusterBlock.java
@@ -0,0 +1,13 @@
+package com.example.createaura.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class AuraCrystalClusterBlock extends Block {
+    public AuraCrystalClusterBlock(Properties properties) {
+        super(properties);
+    }
+}
